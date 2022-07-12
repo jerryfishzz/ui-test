98171e0f5cad15bccd5b328719fee0077752e02f
jerryfishzz <jerryfish@163.com>
2022-07-12 20:40:30 +1200

    Migrate all the sample HTML elements
    feat: Migrate all the sample HTML elements to app

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,125 @@ function App() {
   return (
     <div className="ui text container">
       <h1>UI Test</h1>
+
+      <div>
+        <div id="toggle-terminated" className="ui toggle checkbox">
+          <input type="checkbox" />
+          <label>Show Terminated Employees</label>
+        </div>
+      </div>
+
+      <table className="ui very basic table">
+        <thead>
+          <tr>
+            <th className="collapsing center aligned">
+              <div className="ui checkbox">
+                <input type="checkbox" />
+                <label></label>
+              </div>
+            </th>
+            <th>Name</th>
+            <th>Department</th>
+            <th className="right aligned">Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className="ui checkbox">
+                <input type="checkbox" />
+                <label></label>
+              </div>
+            </td>
+            <td>Alex</td>
+            <td>Sales</td>
+            <td className="right aligned">
+              <span className="ui green label">Active</span>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className="ui checkbox">
+                <input type="checkbox" />
+                <label></label>
+              </div>
+            </td>
+            <td>Brian</td>
+            <td>Marketing</td>
+            <td className="right aligned">
+              <span className="ui green label">Active</span>
+            </td>
+          </tr>
+          <tr data-terminated>
+            <td>
+              <div className="ui checkbox">
+                <input type="checkbox" />
+                <label></label>
+              </div>
+            </td>
+            <td>Caroline</td>
+            <td>Human Resources</td>
+            <td className="right aligned">
+              <span className="ui black label">Terminated</span>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className="ui checkbox">
+                <input type="checkbox" />
+                <label></label>
+              </div>
+            </td>
+            <td>Diana</td>
+            <td>Support</td>
+            <td className="right aligned">
+              <span className="ui green label">Active</span>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className="ui checkbox">
+                <input type="checkbox" />
+                <label></label>
+              </div>
+            </td>
+            <td>Ernest</td>
+            <td>Product</td>
+            <td className="right aligned">
+              <span className="ui green label">Active</span>
+            </td>
+          </tr>
+          <tr data-terminated>
+            <td>
+              <div className="ui checkbox">
+                <input type="checkbox" />
+                <label></label>
+              </div>
+            </td>
+            <td>Fred</td>
+            <td>Accounting</td>
+            <td className="right aligned">
+              <span className="ui black label">Terminated</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div>
+        <button
+          id="bulk-actions"
+          className="ui disabled pointing dropdown button"
+        >
+          <span>
+            <i className="cog icon"></i>
+          </span>
+          <div className="menu">
+            <div className="item">Bulk Action 1</div>
+            <div className="item">Bulk Action 2</div>
+            <div className="item">Bulk Action 3</div>
+          </div>
+        </button>
+      </div>
     </div>
   )
 }
